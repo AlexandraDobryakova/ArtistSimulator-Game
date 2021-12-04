dffd17f9fab0c7708482f1db6fa6ec917839d7e5
Arseny <ars.stuk.v@gmail.com>
2021-12-04 18:20:21 +0500

    Добавил рандомную генерацию контрактов

--- Artist Simulator/Assets/Scripts/Backend scripts/Indicators/Contract.cs
+++ Artist Simulator/Assets/Scripts/Backend scripts/Indicators/Contract.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Contract : Work
 {
@@ -57,6 +58,38 @@ public class Contract : Work
     }
 
     public int GetPercentExecution() => (hoursWorked / HourWorkload) * 100;
+
+    public Contract GetRandomContract()
+    {
+        var random = new System.Random();
+        var difficult = (Difficultys)random.Next(0, Enum.GetValues(typeof(Difficultys)).Length);
+        var randTech = (Player.ArtSkills.Techniques)random.Next(0, Enum.GetValues(typeof(Player.ArtSkills.Techniques)).Length);
+        var randGenre = (Player.ArtSkills.Genres)random.Next(0, Enum.GetValues(typeof(Player.ArtSkills.Genres)).Length);
+        
+        switch (difficult)
+        {
+            //(int)Math.Round(a / 10.0) * 10)
+            case Difficultys.Easy:
+                return new Contract("1", random.Next(1, 6),
+                    (int)Math.Round(random.Next(100, 600) / 10.0) * 10, difficult, randTech, randGenre);
+
+           case Difficultys.Normal:
+               return new Contract("2", random.Next(12, 12 * 2 + 1), 
+                   (int)Math.Round(random.Next(800, 1500) / 100.0) * 100, difficult, randTech, randGenre);  
+
+           case Difficultys.Hard:
+               return new Contract("3", random.Next(12 * 3, 12 * 4 + 1),
+                   (int)Math.Round(random.Next(2500, 5000) / 100.0) * 100, difficult, randTech, randGenre);
+
+           case Difficultys.Crazy:
+               return new Contract("4", random.Next(12 * 5, 12 * 6 + 1),
+                   (int)Math.Round(random.Next(15000, 20000) / 1000.0) * 1000, difficult, randTech, randGenre);
+
+           default:
+               return new Contract("0", 0, 0, difficult, randTech, randGenre);
+
+        }
+    }
 
     private static readonly int energyCostPerHour, satietyCostPerHour, happinessCoef;
     private int hourWorkload, hoursWorked, contractPrice;
